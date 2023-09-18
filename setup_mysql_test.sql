8a6623e6f6234596c70af20f7b5c9042225bf2d9
rugema3 <rugema61@gmail.com>
2023-09-18 21:19:16 +0200

    modifications

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -9,7 +9,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Grant all privileges on hbnb_test_db to hbnb_test
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- Grant SELECT privilege on performance_schema to hbnb_test
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
