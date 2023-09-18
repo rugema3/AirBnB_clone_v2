e92880337681fb743b7e586749602c8c401777a5
rugema3 <rugema61@gmail.com>
2023-09-18 20:05:17 +0200

    an sql script that creates a database, creates a user of that databases and grants the user with some priviledges

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,19 @@
+-- this sql script creates a database, then creates a user of that database
+-- It then sets up the credentials of the user with username and password.
+-- The script also grants priviledges to the uses.
+
+-- Create the hbnb_dev_db database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the hbnb_dev user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
+
