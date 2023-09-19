b446a620da6e72548614d12991ec528cdf0906ff
rugema3 <rugema61@gmail.com>
2023-09-19 22:26:55 +0200

    pushing the upades from city.py and base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,24 +2,36 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), Unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
+            # from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            # storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            # Delete the '__class__' key if it exists in kwargs.
+            if '__class__' in kwargs:
+                del kwargs['__class__']
+
+            # Update the instance's attributes with the values from kwargs.
             self.__dict__.update(kwargs)
 
     def __str__(self):
@@ -31,14 +43,34 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new()
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
+
+        # Copy all attributes from the instance's __dict__ to the dictionary.
         dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+
+        # Remove the '_sa_instance_state' key if it exists in the dictionary.
+        if '_sa_instance_state' in dictionary:
+            del dictionary['_sa_instance_state']
+
+        # Add '__class__' key to the dictionary with the class name.
+        class_name = str(type(self)).split('.')[-1]
+        class_name = class_name.split('\'')[0]
+        dictionary['__class__'] = class_name
+
+        # Convert created_at and updated_at to ISO format.
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+
         return dictionary
+
+    def delete(self):
+        """"Delete the current instance from the storage"""
+        from models import storage
+
+        # Use the storage's delete method to remove the instance
+        storage.delete(self)
--- models/city.py
+++ models/city.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, ForeignKey, Column
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(string(60), nullable=False, ForeignKey("state_id"))
+    
+    def __init__(self, name, state_id):
+        """Define init method."""
+        self.name = name
+        self.state_id = state_id
