1ccae049df29e9645a111fb7a6ad1fdf4a0034ab
rugema3 <rugema61@gmail.com>
2023-09-20 10:58:19 +0200

    updated the db_storage module.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,6 +3,13 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
+rom sqlalchemy.ext.declarative import declarative_base
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 class DBConfig:
     """Database configuration class"""
