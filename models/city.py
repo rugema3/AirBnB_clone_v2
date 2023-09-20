ea165379a8946be0a8ef2acc00980838d762b7d9
rugema3 <rugema61@gmail.com>
2023-09-20 10:13:31 +0200

    updates to state and city modules

--- models/city.py
+++ models/city.py
@@ -2,15 +2,21 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, ForeignKey, Column
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(string(60), nullable=False, ForeignKey("state_id"))
-    
-    def __init__(self, name, state_id):
+    state_id = Column(string(60), nullable=False, ForeignKey("state.id"))
+    places = relationship("Place",
+                          backref="cities",
+                          cascade="all, delete-orphan",
+                          passive_deletes=True)
+
+    def __init__(self, *args, **kwargs):
         """Define init method."""
-        self.name = name
-        self.state_id = state_id
+        super().__init__()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ The state class, contains state name """
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """Getter attribute cities that returns the list of City instances
+            with state_id equals to the current State.id"""
+            from models import storage
+            list_cities = []
+            for city in storage.all("City").values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
