1f38c4b89583d336c2effd9cdcde7f455fec269d
rugema3 <rugema61@gmail.com>
2023-10-07 23:41:50 +0200

    modifications

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,22 +1,52 @@
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $hostname;
-    root   /var/www/html;
-    index  index.html index.htm;
+# web_server_setup.pp
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
+# Install Nginx package
+package { 'nginx':
+  ensure => 'installed',
 }
+
+# Create directories
+file { '/data/web_static/releases/test/':
+  ensure => 'directory',
+}
+
+file { '/data/web_static/shared/':
+  ensure => 'directory',
+}
+
+# Create a fake HTML file
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'file',
+  content => 'Devoops is not that easy',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0644',
+}
+
+# Create a symbolic link
+file { '/data/web_static/current':
+  ensure  => 'link',
+  target  => '/data/web_static/releases/test',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+}
+
+# Define Nginx configuration
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => template('path/to/nginx_config.erb'),
+  notify  => Service['nginx'],
+}
+
+# Notify Nginx to restart when the configuration changes
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => File['/etc/nginx/sites-available/default'],
+}
+
+# Print a message indicating the setup is complete
+notify { 'web_server_setup_message':
+  message => 'Web server setup for web_static deployment is complete.',
+}
+
