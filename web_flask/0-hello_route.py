2034516b5d65bf202aed3c7aa2b5e7b9d7f67b38
rugema3 <rugema61@gmail.com>
2023-10-20 16:29:21 +0200

    complying with pycodestye

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def display():
     """A method that displays hello HBNB!."""
     return “Hello HBNB!”
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
