82a5566b6f60f7e5190193d7bb0fb1ffff044234
rugema3 <rugema61@gmail.com>
2023-10-20 16:23:49 +0200

    displayinga  simple message with flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A module that displays a simple message using flask."""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def display():
+    """A method that displays hello HBNB!."""
+    return “Hello HBNB!”
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
